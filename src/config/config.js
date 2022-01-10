0e6e0a4efdf90afa937b70f1652497fb161db742
tncsource <source@tncitgroup.com>
2022-01-10 10:55:17 +0900

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+   mode : 'development',
+   version : '0',
+   port     : 3035,
+   api_url : "ws://127.0.0.1:17001",
+   address_prefix : "ADC",
+   fee_string : "ADC",
+   fee_string_rpad : 6,
+   creator : "",
+   creator_wif : "",
+   chain_id: "2a622804221ddbd527083eaa7cf8f4468330b508816675ff4a0cab10c3676c7c",		// mainnet
+};
